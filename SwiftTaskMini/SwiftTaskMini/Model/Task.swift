bfcbc0c227b40e7156b22b3493404fd9e453d6dc
Rahul kamra <102607302+rkamra-1404@users.noreply.github.com>
2025-05-07 14:07:57 -0400

    Update Task.swift

--- SwiftTaskMini/SwiftTaskMini/Model/Task.swift
+++ SwiftTaskMini/SwiftTaskMini/Model/Task.swift
@@ -7,8 +7,17 @@
 
 import Foundation
 
-struct Task: Identifiable {
-    let id = UUID()
+struct Task: Identifiable, Codable, Equatable {
+    var id = UUID()
     var title: String
+    var dueDate: Date?
+    var tags: [String] = []
+    var priority: Priority = .high
     var isCompleted: Bool = false
+
+    enum Priority: String, CaseIterable, Codable, Identifiable {
+            case low, medium, high
+
+            var id: String { self.rawValue }
+        }
 }
